e59fdecebf5e9571bf34861780802f848f735f85
rohmanalka <rohmanalka06@gmail.com>
2024-03-27 19:21:42 +0700

    penambahan method baru

--- Praktikum06/Sorting21.java
+++ Praktikum06/Sorting21.java
@@ -10,8 +10,34 @@ public class Sorting21 {
         System.out.println("Data tidak ditemukan");
     }
 
+    public static int binarySearchAsc(int[] arr, int key) {
+        int start = 0, end = arr.length -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } if (arr[mid] < key) {
+                start = mid;
+            } else {
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] daftarNilai = {10, 5, 20, 15, 5, 45};
         sequentialSearch(daftarNilai, 5);
+
+        int[] sortedNilai = {5, 5, 10, 20, 30, 40, 50};
+        int index = binarySearchAsc(daftarNilai, 5);
+
+        if(index != -1) {
+            System.out.println("Data ditemukan pada index ke-" + index);
+        } else {
+            System.out.println("Data tidak ditemukan");
+        }
     }
 }
